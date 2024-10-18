2b161fad50570e32e40144389b3d0d3dd76eda3e
Dhanyashree <dhanyakarkera100@gmail.com>
2024-10-18 16:41:43 +0530

    hyy

--- src/app/data/data.ts
+++ src/app/data/data.ts
@@ -1,79 +1,79 @@
-export const moviedata=[
+[
     {
-      id: 533535,
-      title: "Deadpool & Wolverine",
-      release_date: "2024-07-24",
-      backdrop_path:
+      "id": 533535,
+      "title": "Deadpool & Wolverine",
+      "release_date": "2024-07-24",
+      "backdrop_path":
         "https://image.tmdb.org/t/p/original/yDHYTfA3R0jFYba16jBB1ef8oIt.jpg",
-      poster_path:
+      "poster_path":
         "https://image.tmdb.org/t/p/original/8cdWjvZQUExUUTzyp4t6EDMubfO.jpg",
-      category: "Action",
+      "category": "Action",
     },
     {
-      id: 718821,
-      title: "Twisters",
-      release_date: "2024-07-10",
-      backdrop_path:
+      "id": 718821,
+      "title": "Twisters",
+      "release_date": "2024-07-10",
+      "backdrop_path":
         "https://image.tmdb.org/t/p/original/58D6ZAvOKxlHjyX9S8qNKSBE9Y.jpg",
-      poster_path:
+      "poster_path":
         "https://image.tmdb.org/t/p/original/pjnD08FlMAIXsfOLKQbvmO0f0MD.jpg",
-      category: "Thriller",
+      "category": "Thriller",
     },
     {
-      id: 1022789,
-      title: "Inside Out 2",
-      release_date: "2024-06-11",
-      backdrop_path:
+      "id": 1022789,
+      "title": "Inside Out 2",
+      "release_date": "2024-06-11",
+      "backdrop_path":
         "https://image.tmdb.org/t/p/original/stKGOm8UyhuLPR9sZLjs5AkmncA.jpg",
-      poster_path:
+      "poster_path":
         "https://image.tmdb.org/t/p/original/vpnVM9B6NMmQpWeZvzLvDESb2QY.jpg",
-      category: "Animation",
+      "category": "Animation",
     },
     {
-      id: 519182,
-      title: "Despicable Me 4",
-      release_date: "2024-06-20",
-      backdrop_path:
+      "id": 519182,
+      "title": "Despicable Me 4",
+      "release_date": "2024-06-20",
+      "backdrop_path":
         "https://image.tmdb.org/t/p/original/lgkPzcOSnTvjeMnuFzozRO5HHw1.jpg",
-      poster_path:
+      "poster_path":
         "https://image.tmdb.org/t/p/original/wWba3TaojhK7NdycRhoQpsG0FaH.jpg",
-      category: "Animation",
+      "category": "Animation",
     },
     {
-      id: 573435,
-      title: "Bad Boys: Ride or Die",
-      release_date: "2024-06-05",
-      backdrop_path:
+      "id": 573435,
+      "title": "Bad Boys: Ride or Die",
+      "release_date": "2024-06-05",
+      "backdrop_path":
         "https://image.tmdb.org/t/p/original/3q01ACG0MWm0DekhvkPFCXyPZSu.jpg",
-      poster_path:
+      "poster_path":
         "https://image.tmdb.org/t/p/original/oGythE98MYleE6mZlGs5oBGkux1.jpg",
-      category: "Action",
+      "category": "Action",
     },
     {
-      id: 1160018,
-      title: "Kill",
-      release_date: "2024-07-03",
-      backdrop_path:
+      "id": 1160018,
+      "title": "Kill",
+      "release_date": "2024-07-03",
+      "backdrop_path":
         "https://image.tmdb.org/t/p/original/okVLmXL5y18dfN2R4ufMZEGaeCd.jpg",
-      poster_path:
+      "poster_path":
         "https://image.tmdb.org/t/p/original/m2zXTuNPkywdYLyWlVyJZW2QOJH.jpg",
-      category: "Thriller",
+      "category": "Thriller",
     },
     {
-      id: 762441,
-      title: "A Quiet Place: Day One",
-      release_date: "2024-06-26",
-      backdrop_path:
+      "id": 762441,
+      "title": "A Quiet Place: Day One",
+      "release_date": "2024-06-26",
+      "backdrop_path":
         "https://image.tmdb.org/t/p/original/2RVcJbWFmICRDsVxRI8F5xRmRsK.jpg",
-      poster_path:
+      "poster_path":
         "https://image.tmdb.org/t/p/original/hU42CRk14JuPEdqZG3AWmagiPAP.jpg",
-      category: "Horror",
+      "category": "Horror",
     },
     {
-      id: 831815,
-      title: "Saving Bikini Bottom: The Sandy Cheeks Movie",
-      release_date: "2024-08-01",
-      backdrop_path:
+      "id": 831815,
+      "title": "Saving Bikini Bottom: The Sandy Cheeks Movie",
+      "release_date": "2024-08-01",
+      "backdrop_path":
         "https://image.tmdb.org/t/p/original/hdFIdXwS8FSN2wIsuotjW1mshI0.jpg",
       poster_path:
         "https://image.tmdb.org/t/p/original/30YnfZdMNIV7noWLdvmcJS0cbnQ.jpg",
--- src/app/dramas/dramas.component.ts
+++ src/app/dramas/dramas.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
-import {data} from "../data/data";
+import { MovieService } from '../service/movie.service';
+// import {data} from "../data/data";
 
 @Component({
   selector: 'app-dramas',
@@ -10,9 +11,23 @@ import {data} from "../data/data";
   styleUrl: './dramas.component.css'
 })
 export class DramasComponent {
+  data:any[]=[];
 
-  ngOnInit(){
-    console.log(data);
+
+
+  constructor(private movieService:MovieService){}
+
+  ngOnInit():void{
+    this.fetchData()
     
   }
+
+  fetchData() {
+    this.movieService.getMovies().subscribe((dhanya) => {
+        this.data=dhanya.Search;
+        console.log(this.data);
+    })
+  }
 }
+
+
